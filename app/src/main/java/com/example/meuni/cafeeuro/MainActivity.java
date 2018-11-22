dbf9a98a60c71dfc8094cd0b07c659652139fad6
Hugo Meunier <meunierhugo@hotmail.fr>
2018-11-22 22:09:07 +0100

    commit to db

--- app/src/main/java/com/example/meuni/cafeeuro/MainActivity.java
+++ app/src/main/java/com/example/meuni/cafeeuro/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    //commit to database
     Button btnList;
     Button btnInfo;
     Button btnMap;
